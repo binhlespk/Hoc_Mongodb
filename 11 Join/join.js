505e9043e27f0c5cfcc5fae4945a8cfa95fa79c5
binhlespk <lethanhbinh.spk@gmail.com>
2019-10-06 21:52:15 +0700

    update 11

---
+++ 11 Join/join.js
@@ -0,0 +1,23 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://127.0.0.1:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  dbo.collection('Staff').aggregate([
+    { $lookup:
+       {
+         from: 'products',
+         localField: 'product_id',
+         foreignField: '_id',
+         as: 'orderdetails'
+        // _id : 'number,'
+        // name : 'name'
+       }
+     }
+    ]).toArray(function(err, res) {
+    if (err) throw err;
+    console.log(JSON.stringify(res));
+    db.close();
+  });
+});
